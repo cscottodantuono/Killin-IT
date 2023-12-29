f9650cfcacb510253f581dd331c62f3dd99b15e6
cscottodantuono <c.scottodantuono@me.com>
2023-12-28 19:45:04 -0500

    cleaned up code, SPI freq. now 40mhz all running smooth

--- src/GIFDraw.h
+++ src/GIFDraw.h
@@ -9,7 +9,7 @@ TFT_eSPI tft = TFT_eSPI();
 #define DISPLAY_WIDTH  tft.width()
 #define DISPLAY_HEIGHT tft.height()
 #define BUFFER_SIZE 500            // Optimum is >= GIF width or integral division of width
-#define USE_DMA
+//#define USE_DMA
 
 #ifdef USE_DMA
   uint16_t usTemp[2][BUFFER_SIZE]; // Global to support DMA use
